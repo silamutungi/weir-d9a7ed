18a727782ae294baae37872012ad597f22a4b2d4
Sila Mutungi <silamutungi@users.noreply.github.com>
2026-04-15 10:36:03 -0700

    Add src/components/ui/label.tsx

---
+++ src/components/ui/label.tsx
@@ -0,0 +1,15 @@
+import { forwardRef, type ComponentPropsWithoutRef, type ElementRef } from 'react'
+import * as LabelPrimitive from '@radix-ui/react-label'
+import { cn } from '../../lib/utils'
+
+const Label = forwardRef<ElementRef<typeof LabelPrimitive.Root>, ComponentPropsWithoutRef<typeof LabelPrimitive.Root>>(({ className, ...props }, ref) => (
+  <LabelPrimitive.Root
+    ref={ref}
+    className={cn('text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70', className)}
+    style={{ color: 'var(--color-text)' }}
+    {...props}
+  />
+))
+Label.displayName = LabelPrimitive.Root.displayName
+
+export { Label }
